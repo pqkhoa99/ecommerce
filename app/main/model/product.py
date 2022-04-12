063501c2ae0476a5dfb11d1a4a211fcc607a0e95
khoaphan <phanquockhoa99@gmail.com>
2022-04-13 05:45:26 +0700

    product

--- app/main/model/product.py
+++ app/main/model/product.py
@@ -3,8 +3,7 @@ from .. import db
 class Product(db.Model):
     __tablename__ = 'product'
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    product_id = db.Column(db.String(100), unique=True)
+    product_id = db.Column(db.String(100),  primary_key=True, unique=True)
     name = db.Column(db.String(255), unique=True, nullable=False)
     description = db.Column(db.String(255), nullable=True)
     price = db.Column(db.Integer, nullable=False)
