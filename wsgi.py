594d5686b8dc6e98a9fc3f7ef076bca427f1678f
khoaphan <phanquockhoa99@gmail.com>
2022-04-13 09:13:27 +0700

    edit wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -24,15 +24,5 @@ manager.add_command('db', MigrateCommand)
 def run():
     app.run()
 
-
-@manager.command
-def test():
-    """Runs the unit tests."""
-    tests = unittest.TestLoader().discover('app/test', pattern='test*.py')
-    result = unittest.TextTestRunner(verbosity=2).run(tests)
-    if result.wasSuccessful():
-        return 0
-    return 1
-
 if __name__ == '__main__':
     manager.run()
